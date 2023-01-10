3227f013cc7f640804489eecc0d0a4abdb5b9253
becahp <bcasamo@gmail.com>
2023-01-10 12:05:14 -0300

    Ajustando header

--- header.php
+++ header.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The header.
  *
@@ -14,26 +15,26 @@
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?> <?php twentytwentyone_the_html_classes(); ?>>
+
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>" />
-	<meta name="viewport" content="width=device-width, initial-scale=1" />
-	<?php wp_head(); ?>
+  <meta charset="<?php bloginfo('charset'); ?>" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
-<div id="page" class="site template-base">
-	
-	<nav id="skip-links">
-      <div class="skip-links" aria-label="Acesso rápido"><a accesskey="1" href="#main-content">Ir para o Conteúdo<strong class="accesskey">1</strong></a><a accesskey="2" href="#main-navigation">Ir para para Navegação<strong
-            class="accesskey">2</strong></a><a accesskey="3" href="#main-searchbox">Ir para para Busca<strong class="accesskey">3</strong></a>
+  <?php wp_body_open(); ?>
+  <div id="page" class="site template-base">
+
+    <nav id="skip-links">
+      <div class="skip-links" aria-label="Acesso rápido"><a accesskey="1" href="#main-content">Ir para o Conteúdo<strong class="accesskey">1</strong></a><a accesskey="2" href="#main-navigation">Ir para para Navegação<strong class="accesskey">2</strong></a><a accesskey="3" href="#main-searchbox">Ir para para Busca<strong class="accesskey">3</strong></a>
       </div>
     </nav>
 
-	<header class="br-header large" id="header" data-sticky="data-sticky">
+    <header class="br-header large" id="header" data-sticky="data-sticky">
       <div class="container-lg">
         <div class="header-top">
-          <div class="header-logo"><img src="<?php echo(esc_url( get_template_directory_uri() ) . '/assets/images/govbr-logo-large.png')?>" alt="logo" />
+          <div class="header-logo"><img src="<?php echo (esc_url(get_template_directory_uri()) . '/assets/images/govbr-logo-large.png') ?>" alt="logo" />
             <div class="header-sign">Ministério da Ciência, Tecnologia e Inovações</div>
           </div>
           <div class="header-actions">
@@ -43,8 +44,7 @@
               <div class="br-list">
                 <div class="header">
                   <div class="title">Acesso Rápido</div>
-                </div><a class="br-item" href="https://www.gov.br/pt-br/orgaos-do-governo">Órgãos do Governo</a><a class="br-item" href="http://www.acessoainformacao.gov.br/">Acesso à Informação</a><a class="br-item"
-                  href="http://www4.planalto.gov.br/legislacao">Legislação</a><a class="br-item" href="https://www.gov.br/governodigital/pt-br/acessibilidade-digital">Acessibilidade</a>
+                </div><a class="br-item" href="https://www.gov.br/pt-br/orgaos-do-governo">Órgãos do Governo</a><a class="br-item" href="http://www.acessoainformacao.gov.br/">Acesso à Informação</a><a class="br-item" href="http://www4.planalto.gov.br/legislacao">Legislação</a><a class="br-item" href="https://www.gov.br/governodigital/pt-br/acessibilidade-digital">Acessibilidade</a>
               </div>
             </div><span class="vertical mx-half mx-sm-1"></span>
             <div class="header-functions dropdown">
@@ -54,11 +54,11 @@
                 <div class="header">
                   <div class="title">Funcionalidades do Sistema</div>
                 </div>
-				<div class="align-items-center br-item">
+                <div class="align-items-center br-item">
                   <button id="increase-font" class="br-button circle small" type="button" accesskey="1"><b>A+</b><span class="text">Aumentar Fonte</span>
                   </button>
                 </div>
-				<div class="align-items-center br-item">
+                <div class="align-items-center br-item">
                   <button id="decrease-font" class="br-button circle small" type="button" accesskey="2"><b>A-</b><span class="text">Diminuir Fonte</span>
                   </button>
                 </div>
@@ -72,7 +72,8 @@
               <button class="br-button circle" type="button" aria-label="Abrir Busca" data-toggle="search" data-target=".header-search"><i class="fas fa-search" aria-hidden="true"></i>
               </button>
             </div>
-			<?php //if(!is_user_logged_in()) : ?>
+            <?php //if(!is_user_logged_in()) : 
+            ?>
             <div class="header-login">
               <div class="header-sign-in">
                 <button class="br-button sign-in small" type="button" onclick="myFunctionEntrar()"><i class="fas fa-user" aria-hidden="true"></i><span class="d-sm-inline">Entrar</span>
@@ -136,7 +137,8 @@
                 </div>
               </div>
             </div>
-			<?php //endif; ?>
+            <?php //endif; 
+            ?>
           </div>
         </div>
         <div class="header-bottom">
@@ -156,18 +158,21 @@
                             <button class="br-button circle" type="button" arial-label="Fechar o menu" data-dismiss="menu"><i class="fas fa-times" aria-hidden="true"></i>
                             </button>
                           </div>
-                          <div class="menu-title"><img src="<?php echo(esc_url( get_template_directory_uri() ) . '/assets/images/govbr-logo-large.png')?>" alt="Imagem ilustrativa" /></div>
+                          <div class="menu-title"><img src="<?php echo (esc_url(get_template_directory_uri()) . '/assets/images/govbr-logo-large.png') ?>" alt="Imagem ilustrativa" /></div>
                         </div>
                         <nav class="menu-body">
                           <div><a class="menu-item" href="<?php echo home_url() ?>">
-                            <span class="content">Início</span></a>
+                              <span class="content">Início</span></a>
                           </div>
-                          <div><a class="menu-item" href="<?php echo home_url() ?>/#contato"><span class="content">Contato</span></a>
+                          <div><a class="menu-item" href="<?php echo home_url() ?>/sobre"><span class="content">Sobre</span></a>
+                          </div>
+                          <div><a class="menu-item" href="<?php echo home_url() ?>/cadastro"><span class="content">Faça parte da Rede</span></a>
+                          </div>
+                          <div><a class="menu-item" href="<?php echo home_url() ?>/contact"><span class="content">Contato</span></a>
                           </div>
                         </nav>
                         <div class="menu-footer">
-                          <div class="menu-links"><a href="https://www.gov.br/pt-br/orgaos-do-governo">Órgãos do Governo&nbsp;<i class="fas fa-external-link-square-alt" aria-hidden="true"></i></a><a href="http://www4.planalto.gov.br/legislacao">Legislação&nbsp;<i
-                                class="fas fa-external-link-square-alt" aria-hidden="true"></i></a></div>
+                          <div class="menu-links"><a href="https://www.gov.br/pt-br/orgaos-do-governo">Órgãos do Governo&nbsp;<i class="fas fa-external-link-square-alt" aria-hidden="true"></i></a><a href="http://www4.planalto.gov.br/legislacao">Legislação&nbsp;<i class="fas fa-external-link-square-alt" aria-hidden="true"></i></a></div>
                           <div class="menu-social">
                             <div class="text-semi-bold mb-1">Título</div>
                             <div class="sharegroup">
@@ -215,9 +220,9 @@
       </div>
     </header>
 
-	<?php //get_template_part( 'template-parts/header/site-header' ); ?>
+    <?php //get_template_part( 'template-parts/header/site-header' ); 
+    ?>
 
-	<div id="content" class="site-content">
-		<div id="primary" class="content-area">
-			<main id="main" class="site-main d-block" role="main">
-
+    <div id="content" class="site-content">
+      <div id="primary" class="content-area">
+        <main id="main" class="site-main d-block" role="main">
