a0f48b6900eeb8aa65d63c6792ce9514d1e005e3
mateuswetah <mateus.m.luna@gmail.com>
2023-03-23 16:06:24 -0300

    Reverte mudanças feitas no breadcrumb para tratarmos disso em outra issue.

--- single-rede-6.php
+++ single-rede-6.php
@@ -23,12 +23,12 @@ while (have_posts()) :
 	/* Pegar a taxonomia do post
 	https://developer.wordpress.org/reference/functions/get_the_terms/#comment-2587 */
 	$term_obj_list = get_the_terms(get_the_id(), $categoria_rede);
-	$terms_string = isset(wp_list_pluck($term_obj_list, 'name')[0]) ? wp_list_pluck($term_obj_list, 'name')[0] : '';
-	$term_slug = isset(wp_list_pluck($term_obj_list, 'slug')[0]) ? wp_list_pluck($term_obj_list, 'slug')[0] : '';
+	$terms_string = wp_list_pluck($term_obj_list, 'name')[0];
+	$term_slug = wp_list_pluck($term_obj_list, 'slug')[0];
 
 	echo "<div class=\"container-lg d-block\">";
 	echo "<div class=\"breadcrumb-post\" style=\"margin-left: -24px;\">";
-	echo do_shortcode('[shortcode_breadcrumb_redes rede_slug="' . $rede_slug . '" rede_name="' . $rede_name . (!empty($terms_slug) ? '" categoria_slug="' . $term_slug : '') . (!empty($terms_string) ? '" categoria_name="' . $terms_string : '') . '" categoria_rede="' . $categoria_rede . '"]');
+	echo do_shortcode('[shortcode_breadcrumb_redes rede_slug="' . $rede_slug . '" rede_name="' . $rede_name . '" categoria_slug="' . $term_slug . '" categoria_name="' . $terms_string . '" categoria_rede="' . $categoria_rede . '"]');
 	echo "</div>";
 	echo "</div>";
 
