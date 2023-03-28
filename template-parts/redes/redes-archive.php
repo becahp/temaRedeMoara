3b833bf50b5bb9034f6180682eb8183f960f10ef
mateuswetah <mateus.m.luna@gmail.com>
2023-03-28 15:34:59 -0300

    Archive placeholder e link ajustados.

--- template-parts/redes/redes-archive.php
+++ template-parts/redes/redes-archive.php
@@ -10,18 +10,18 @@
  */
 
 ?>
-<div class="col-md-3 archive-rcc">
+<div class="col-md-3 archive-rcc archive-moara">
 	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 		<?php
 			if ( has_post_thumbnail() ) {
-				the_post_thumbnail( 'medium-thumbnail' );
+				the_post_thumbnail( 'thumbnail_redes_retangular' );
 			} else {
-				echo '<img src="' . get_template_directory_uri() . '/assets/images/logos/moara.png' . '" class="attachment-medium-thumbnail size-medium-thumbnail wp-post-image" alt="" decoding="async" style="width:100%;height:100.58%;max-width:107px;" width="107" height="86">';
+				echo '<img src="' . get_template_directory_uri() . '/assets/images/logos/moara_placeholder.png' . '" class="attachment-thumbnail_redes_retangular size-thumbnail_redes_retangular wp-post-image" alt="" decoding="async" style="width:100%;height:100.58%;max-width:450px;" width="450" height="250">';
 			}
 		?>
-		<h2><a class="post-title" href="<?php the_field('url'); ?>" target="_blank"><?php the_title(); ?></a></h2>
+		<h2><a class="post-title" href="<?php the_permalink(); ?>" target="_blank"><?php the_title(); ?></a></h2>
 		<!--<p class="post-excerpt"><?php #echo get_the_excerpt(); ?></p> -->
 		<p class="post-excerpt"><?php echo wp_trim_words(get_field("texto_hover"), 30) ?></p>
-		<a class="moretext" href="<?php the_field('url'); ?>" target="_blank">Veja o projeto 🡪</a>
+		<a class="moretext" href="<?php the_permalink(); ?>" target="_blank">Veja o projeto &#10140;</a>
 	</article><!-- #post-${ID} -->
 </div>
