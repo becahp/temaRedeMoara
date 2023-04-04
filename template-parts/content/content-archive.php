7b1c1a5b7a3014f926bc2c61fe9b51804c4d8a09
mateuswetah <mateus.m.luna@gmail.com>
2023-04-04 15:00:58 -0300

    Adiciona classe da rede moara e placeholders para todos os archives.

--- template-parts/content/content-archive.php
+++ template-parts/content/content-archive.php
@@ -10,12 +10,18 @@
  */
 
 ?>
-<div class="col-md-3 archive-rcc">
+<div class="col-md-3 archive-rcc archive-moara">
 	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-		<?php the_post_thumbnail('medium-thumbnail'); ?>
+	<?php
+			if ( has_post_thumbnail() ) {
+				the_post_thumbnail( 'thumbnail_redes_retangular' );
+			} else {
+				echo '<img src="' . get_template_directory_uri() . '/assets/images/logos/moara_placeholder.png' . '" class="attachment-thumbnail_redes_retangular size-thumbnail_redes_retangular wp-post-image" alt="" decoding="async" style="width:100%;height:100.58%;max-width:450px;" width="450" height="250">';
+			}
+		?>
 		<h2><a class="post-title" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 		<!--<p class="post-excerpt"><?php #echo get_the_excerpt(); ?></p> -->
 		<p class="post-excerpt"><?php echo wp_trim_words(get_the_content(), 30) ?></p>
-		<a class="moretext" href="<?php the_permalink(); ?>">Continue lendo 🡪</a>
+		<a class="moretext" href="<?php the_permalink(); ?>">Veja o projeto &#10140;</a>
 	</article><!-- #post-${ID} -->
 </div>
